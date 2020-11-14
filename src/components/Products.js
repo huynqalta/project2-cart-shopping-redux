cec54fca606ba0430d2e3e12b7b8e7bbb3447be5
huynqalta <huynq@alta.com.vn>
2020-11-14 11:49:41 +0700

    push lan 1

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,32 +1,16 @@
 import React, { Component } from 'react'
-import { connect } from 'react-redux'
-import Product from './Product'
+
 
  class Products extends Component {
     render() {
-        var {products}=this.props;
         return (
             <section className="section">
                         <h1 className="section-heading">Danh Sách Sản Phẩm</h1>
                         <div className="row">
-                            {this.ShowProduct(products)}
+                            {this.props.children}
                         </div>
             </section>
         )
     };
-    ShowProduct (products) {
-        var result=null;
-        if(products.length>0){
-            result = products.map((product,index)=>{
-                return <Product key={index} product={product} />
-            })
-        }
-        return result;
-     };
 }
-const mapStateToProps=state=>{
-    return {
-        products:state.products,
-    }
-}
-export default connect(mapStateToProps,null)(Products);
+export default Products;
--- src/containers/ProductsContainer.js
+++ src/containers/ProductsContainer.js
@@ -1,18 +1,44 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import Products from '../components/Products';
-
+import Product from '../components/Product';
+import PropsTypes from 'prop-types';
  class ProductsContainer extends Component {
     render() {
         var {products}=this.props;
         return (
-            <Products products={products} />
+            <Products>
+                {this.ShowProduct(products)}
+            </Products>
         )
     };
+    ShowProduct (products) {
+        var result=null;
+        if(products.length>0){
+            result = products.map((product,index)=>{
+                return <Product key={index} product={product} />
+            })
+        }
+        return result;
+     };
+}
+ProductsContainer.propTypes ={
+    products : PropsTypes.arrayOf(
+        PropsTypes.shape({
+            id:PropsTypes.number,
+            name:PropsTypes.string,
+            image:'https://www.ifd-pforzheim.de/images/ifdpforzheimde/714-iphone-11-pro-max-apple-3342.jpg',
+            description:PropsTypes.string,
+            price:'510',
+            inventory:15,
+            rating:5
+        })
+    ).isRequired
 }
 const mapStateToProps=state=>{
     return {
         products:state.products,
     }
 }
+
 export default connect(mapStateToProps,null)(ProductsContainer);
